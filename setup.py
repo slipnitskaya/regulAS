b404d993bf76d996dd9834e87948b2b8dd957adf
slipnitskaya <lipnitskaya.sofya@gmail.com>
2021-07-28 22:29:50 +0200

    Add packaging script

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+
+import regulAS
+
+from setuptools import setup
+
+
+setup(
+    name='regulAS',
+    version=regulAS.__version__,
+    packages=['regulAS', 'regulAS.core', 'regulAS.utils', 'regulAS.reports', 'regulAS.persistence'],
+    package_data={'': [os.path.join('conf', '*.yaml')]},
+    url='https://github.com/slipnitskaya/regulAS',
+    author='Sofya Lipnitskaya',
+    author_email='lipnitskaya.sofya@gmail.com',
+    description='',
+    python_requires='>=3.7',
+    install_requires=[
+        'networkx>=2.5',
+        'numpy>=1.19.2',
+        'sklearn>=0.24.2',
+        'pandas>=1.2.2',
+        'matplotlib>=3.1.3',
+        'omegaconf>=2.1.0',
+        'hydra-core>=1.1.0',
+        'hydra-colorlog>=1.1.0',
+        'sqlalchemy>=1.4.17',
+        'msgpack>=1.0.2',
+        'msgpack-numpy>=0.4.6.1'
+    ]
+)
